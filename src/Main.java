d4858d885dada6c1f85593fdeac4dd477eb7c0e7
Moxiao <limoxiao1998@gmail.com>
2025-10-23 09:50:17 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import com.github.raftimpl.raft.example.client.ClientMain_Test;
+
+public class Main {
+    public static void main(String[] args) {
+        ClientMain_Test.testRaft("http://114.96.84.122:8052/","raft","vaule");
+    }
+}
